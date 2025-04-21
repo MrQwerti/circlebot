902e8a4b84ccd3fa0194f7ce2a224b910a8e7fb8
MrQwerti <147913450+MrQwerti@users.noreply.github.com>
2025-04-21 20:45:30 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,22 +1,14 @@
+TEMP_DIR=$(mktemp -d)
 REPO_URL="https://github.com/MrQwerti/circlebot.git"
-DIR_NAME="circlebot"
 
-# Клонируем или обновляем
-if [ -d "$DIR_NAME" ]; then
-  echo "📁 Папка $DIR_NAME уже существует, обновляю..."
-  cd "$DIR_NAME"
-  git pull
-else
-  echo "📥 Клонирую репозиторий..."
-  git clone "$REPO_URL"
-  cd "$DIR_NAME"
-fi
+echo "📥 Клонирую репозиторий во временную папку..."
+git clone --depth 1 "$REPO_URL" "$TEMP_DIR"
 
-# Обновляем pip и устанавливаем зависимости
+cd "$TEMP_DIR"
+
 echo "📦 Устанавливаю зависимости..."
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 
-# Запуск бота
 echo "🚀 Запускаю Telegram-бота..."
 python3 main.py

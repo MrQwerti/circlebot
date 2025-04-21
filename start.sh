051f75a4567b6f315d45c8c85341393270be1fa9
MrQwerti <147913450+MrQwerti@users.noreply.github.com>
2025-04-21 20:35:53 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,22 @@
-git clone https://github.com/MrQwerti/circlebot.git
-cd circlebot
-pip install -r requirements.txt
+REPO_URL="https://github.com/MrQwerti/circlebot.git"
+DIR_NAME="circlebot"
+
+# Клонируем или обновляем
+if [ -d "$DIR_NAME" ]; then
+  echo "🔁 Обновляю код из GitHub..."
+  cd "$DIR_NAME"
+  git pull
+else
+  echo "📥 Клонирую репозиторий..."
+  git clone "$REPO_URL"
+  cd "$DIR_NAME"
+fi
+
+# Установка зависимостей
+echo "📦 Установка зависимостей..."
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+
+# Запуск бота
+echo "🚀 Запускаю Telegram-бота..."
 python3 main.py
